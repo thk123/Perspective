ced64c4f0e699367f7bd7fd764b97416a3bf0ade
kbarrett <x_kimmi_13_x@hotmail.co.uk>
2012-11-12 23:29:53 +0000

    initialising a Player & detecting movement from update

--- Perspective/Perspective/Game1.cs
+++ Perspective/Perspective/Game1.cs
@@ -19,6 +19,8 @@ namespace Perspective
         GraphicsDeviceManager graphics;
         SpriteBatch spriteBatch;
 
+        Player player;
+
         public Game1()
         {
             graphics = new GraphicsDeviceManager(this);
@@ -38,6 +40,8 @@ namespace Perspective
             // TODO: Add your initialization logic here
 
             base.Initialize();
+
+            player = new Player(new Position(1));
         }
 
         /// <summary>
@@ -74,6 +78,7 @@ namespace Perspective
 
             // TODO: Add your update logic here
 
+            player.detectInput(Keyboard.GetState());
             base.Update(gameTime);
         }
 
